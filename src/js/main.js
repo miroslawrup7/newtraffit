a0d6af5ca5a4674ca339b34bf0e3aefd26e3e370
Mirek Rup <miroslawrup7@gmail.com>
2023-03-29 20:43:13 +0200

    initial

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+const resizeIFrameToFitContent = () =>{
+    
+    const iframes = document.querySelector("iframe");
+
+    iframes.width = iframes.contentWindow.document.body.scrollWidth;
+    iframes.height = iframes.contentWindow.document.body.scrollHeight;
+
+    console.log(iframes.width);
+    console.log(iframes.height);
+}
+
+const resize = () => {
+    
+    iframes.height = "100%";
+}
+
+
+
+
+// setTimeout(resizeIFrameToFitContent, 3000);
+
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", resizeIFrameToFitContent);
+// btn.addEventListener("click", resize);
+
+
+// http://localhost:3000/
