4aed07ec667c58e208aa01fa20a05cf4e8f24c38
Mirek Rup <miroslawrup7@gmail.com>
2023-04-02 13:59:48 +0200

    test34

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,3 @@
-const iframe1 = document.querySelector("#iframe_1");
-
-const resizeIFrameToFitContent_1 = () => { 
-    iframe1.height = iframe1.contentWindow.document.body.scrollHeight;
-}
-
-const btn1 = document.querySelector("#btn_1");
-btn1.addEventListener("click", resizeIFrameToFitContent_1);
-
-
-
-const iframe2 = document.querySelector("#iframe_2");
-
-const resizeIFrameToFitContent_2 = () => { 
-    iframe2.height = iframe2.contentWindow.document.body.scrollHeight;
-}
-
-const btn2 = document.querySelector("#btn_2");
-btn2.addEventListener("click", resizeIFrameToFitContent_2);
-
-
 window.addEventListener('message', function(event) {
     console.log("Message received from the child: " + event.data); // Message received from child
 
