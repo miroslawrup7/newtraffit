4a5c8dab4bd2f77ba7bb6241ab24423a39c2e1fd
Mirek Rup <miroslawrup7@gmail.com>
2023-04-03 10:10:16 +0200

    test37

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 const iframe = document.querySelector("#iframe");
 
 window.addEventListener('message', function(event) {
-    console.log("Message received from the child: " + event.data);
-
     iframe.height = event.data;
 });
 
